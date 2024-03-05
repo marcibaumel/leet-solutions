4ef2cfcb68fd4274ca6b82f560d0fa61ab1c59d9
Baumel Márton <marcibaumel@gmail.com>
2024-03-05 07:31:21 +0100

    Two Sum finished

--- 1-Two Sum/app.ts
+++ 1-Two Sum/app.ts
@@ -1,6 +1,6 @@
 export function twoSum(nums: number[], target: number): number[] {
     const numMap: Map<number, number> = new Map();
-    for (let i = 0; nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         const dif = target - nums[i];
         if (numMap.has(dif)) {
             return [numMap.get(dif), i];
