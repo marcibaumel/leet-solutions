0461ef6e4b33a1f5d96e94e9d7768f8ffa328332
Baumel Márton <marcibaumel@gmail.com>
2024-03-05 08:44:23 +0100

    finished

--- 35 - Search Insert Position/tests/app.test.ts
+++ 35 - Search Insert Position/tests/app.test.ts
@@ -3,5 +3,9 @@ import { searchInsert } from "../app";
 describe('Search Insert Position tests', () => {
     it('Should return the parameter number', () => {
         expect(searchInsert([1,3,5,6], 5)).toEqual(2);
+        expect(searchInsert([1,3,5,6], 2)).toEqual(1);
+        expect(searchInsert([1,3,5,6], 0)).toEqual(0);
+        expect(searchInsert([1,3,5,6], 57)).toEqual(4);
+        expect(searchInsert([1,3,5,6], 7)).toEqual(4);
     })
 })
