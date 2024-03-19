e651b284aeab048b82f917034c54a5b4c248cf1a
Marci <marcibaumel@gmail.com>
2024-03-19 08:38:14 +0100

    C# solution for Two Sum

---
+++ CSharp/LeetSolutions/Problems/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems
+{
+    public class TwoSum
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (numIndices.ContainsKey(complement))
+                {
+                    return new int[] { numIndices[complement], i };
+                }
+                if (!numIndices.ContainsKey(nums[i]))
+                {
+                    numIndices.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
