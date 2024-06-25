80df5906d5121b40bc8583e4641e78ff7d73e136
Marci <marcibaumel@gmail.com>
2024-06-25 08:19:22 +0200

    clean

---
+++ Tests/Test_IslandPerimeter.cs
@@ -0,0 +1,18 @@
+﻿using Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsTests
+{
+    public class Test_IslandPerimeter
+    {
+        [Fact]
+        public void IslandPerimeter_CorrectSolution_v0()
+        {
+            Assert.Equal(4, IslandPerimeter.Solution([[1]]));
+        }
+    }
+}
