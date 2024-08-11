c637565f592afdac33b9ae9ac4d4527dd69f0f85
Marci <marcibaumel@gmail.com>
2024-08-11 15:41:52 +0200

    Bun update

--- sample/tests/app.test.ts
+++
@@ -1,9 +0,0 @@
-/*
-import { sample } from "../app";
-
-describe('Add function test', () => {
-    it('Should return the parameter number', () => {
-        expect(sample(1)).toEqual(1);
-    })
-})
-*/
